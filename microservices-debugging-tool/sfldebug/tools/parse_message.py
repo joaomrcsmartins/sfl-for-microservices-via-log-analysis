7bc8d26f408ea309560abb316fb33cff1e13b454
João Martins <up201707311@fe.up.pt>
2022-03-23 19:48:07 +0000

    Add entity parsing and fixed message parsing

--- microservices-debugging-tool/sfldebug/tools/parse_message.py
+++ microservices-debugging-tool/sfldebug/tools/parse_message.py
@@ -1,10 +1,11 @@
 import json
 from pika.channel import Channel
-from pika.spec import BasicProperties,Basic
- 
-from sfldebug.tools.entity import build_entity, parse_entities
+from pika.spec import BasicProperties, Basic
 
-entities = []
+from sfldebug.tools.entity import build_entity, parse_unique_entities
+
+entities = set()
+
 
 def parse_mq_message(channel: Channel, method: Basic.Deliver, properties: BasicProperties, body):
     """Callback to parse messages coming from MQ channel.
@@ -15,13 +16,19 @@ def parse_mq_message(channel: Channel, method: Basic.Deliver, properties: BasicP
         properties (pika.spec.BasicProperties): AMQP specification (ignored)
         body (any): contents of the message
     """
+    del channel, method, properties  # ignore unused arguments
     json_body = json.loads(body)
-    entity = build_entity(json_body)
-    entities.append(entity)
+    print(json.dumps(json_body, indent=2, sort_keys=True))
+    log_entities = build_entity(json_body)
+    entities.update(log_entities)
+
 
 def flush_mq_messages():
     """Once the connection is finished, parse collected entities and write to file"""
-    parsed_entities = parse_entities(entities)
-    with open('entities-records.json','w') as f:
-        json_entities = {"entities": parsed_entities}
-        f.write(json.dumps(json_entities, indent=2, sort_keys=True))
+    parsed_entities = parse_unique_entities(entities)
+    with open('entities-records.json', 'w', encoding='utf-8') as file:
+
+        json_entities = {"entities": []}
+        for entity in parsed_entities:
+            json_entities['entities'].append(entity.__dict__)
+        file.write(json.dumps(json_entities, indent=2, sort_keys=True))
