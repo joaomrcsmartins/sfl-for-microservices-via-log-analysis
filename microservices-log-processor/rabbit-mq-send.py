de64850dc0995e686e11d5b567dcb54f58de8e24
João Martins <up201707311@fe.up.pt>
2022-03-23 15:23:51 +0000

    Fix dummy script and comment conf while developing

--- microservices-log-processor/rabbit-mq-send.py
+++ microservices-log-processor/rabbit-mq-send.py
@@ -1,16 +1,19 @@
-import pika
+import pika, os
+
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
 # extracted from https://www.ibm.com/docs/en/zos/2.1.0?topic=problems-example-log-file
-log_file = open('data/rabbitmq/ibmlogexamples.log','r')
+with open(os.path.join(__location__, 'data/rabbitmq/ibmlogexamples.log'),'r') as log_file:
 
-channel.exchange_declare(exchange='logstash-input', exchange_type='direct')
+    channel.exchange_declare(exchange='logstash-input', exchange_type='direct')
 
-for line in log_file.readlines():
-    channel.basic_publish(exchange='logstash-input', routing_key='logstash-input', body=line)
-    print("Sent :: %r" % line)
+    for line in log_file.readlines():
+        channel.basic_publish(exchange='logstash-input', routing_key='logstash-input', body=line)
+        print("Sent :: %r" % line)
 
-connection.close()
+    connection.close()
